1a952878be3f2ae96f82b16ca4ea40feb86f6f95
kigh-ota <kigh.ota@gmail.com>
2017-12-25 21:35:49 +0900

    move a test class

--- src/test/java/nes/cpu/CPUTest.java
+++ src/test/java/nes/cpu/CPUTest.java
@@ -1,11 +1,10 @@
-package cpu;
+package nes.cpu;
 
-import nes.cpu.CPU;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class _CPUTest {
+class CPUTest {
     @Test
     void getAddressTest() {
         int address = CPU.getAddress((byte)0x01, (byte)0x20);
